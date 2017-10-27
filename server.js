5c8900f4f9df10abb4e7f71694fa28acf84cc993
inder <sainiinder08@gmail.com>
2017-10-27 06:09:17 +0530

    about.html

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const http = require('http');
 const url = require('url');
 const path = require('path');
@@ -45,52 +44,4 @@ http.createServer(function(req, res){
 		res.end();
 	}
 
-=======
-const http = require('http');
-const url = require('url');
-const path = require('path');
-const fs = require('fs');
-
-const mimeTypes = {
-	"html": "text/html",
-	"jpeg": "image/jpeg",
-	"jpg": "image/jpg",
-	"png": "image/png",
-	"js": "text/javascript",
-	"css": "text/css"
-};
-
-http.createServer(function(req, res){
-	var uri = url.parse(req.url).pathname;
-	var fileName = path.join(process.cwd(), unescape(uri));
-	console.log('Loading '+ uri);
-	var stats;
-
-	try{
-		stats = fs.lstatSync(fileName);
-	} catch(e){
-		res.writeHead(404, {'Content-type': 'text/plain'});
-		res.write('404 Not Found\n');
-		res.end();
-		return;
-	}
-
-	if(stats.isFile()){
-		var mimeType = mimeTypes[path.extname(fileName).split(".").reverse()[0]];
-		res.writeHead(200, {'Content-type': mimeType});
-
-		var fileStream = fs.createReadStream(fileName);
-		fileStream.pipe(res);
-	} else if(stats.isDirectory()) {
-		res.writeHead(302, {
-			'Location': 'index.html'
-		});
-		res.end();
-	} else {
-		res.writeHead(500, {'Content-type':'text/plain'});
-		res.write('500 Internal Error\n');
-		res.end();
-	}
-
->>>>>>> 457f134e6c49abb2abe8bbefe3b2f17d0fe67b73
 }).listen(3000);
